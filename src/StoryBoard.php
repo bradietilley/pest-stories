dd5dded943b24f51913243983ae2bb67ec76f3e7
Bradie <44430471+bradietilley@users.noreply.github.com>
2022-12-30 20:36:16 +0800

    feat: add toggle to choose dataset or non-dataset testing

--- src/StoryBoard.php
+++ src/StoryBoard.php
@@ -2,8 +2,12 @@
 
 namespace BradieTilley\StoryBoard;
 
+use PHPUnit\Framework\TestCase;
+
 class StoryBoard extends Story
 {
+    protected static bool $datasetsEnabled = false;
+
     /**
      * Create test cases for all tests
      */
@@ -11,10 +15,31 @@ class StoryBoard extends Story
     {
         $stories = $this->allStories();
 
-        foreach ($stories as $story) {
-            $story->test();
+        if (static::$datasetsEnabled) {
+            $function = Story::getTestFunction();
+
+            $function($this->getName(), function (Story $story) {
+                /** @var Story $story */
+                /** @var TestCase $this */
+
+                $story->setTest($this)->boot()->assert();
+            })->with($stories);
+        } else {
+            foreach ($stories as $story) {
+                $story->test();
+            }
         }
 
         return $this;
+    }
+
+    public static function useDatasets(): void
+    {
+        static::$datasetsEnabled = true;
+    }
+
+    public static function dontUseDatasets(): void
+    {
+        static::$datasetsEnabled = false;
     }
 }
