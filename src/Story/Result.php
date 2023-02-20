80a8afc072610c68f8f99970b07dbc04ce61a5e2
Bradie <44430471+bradietilley@users.noreply.github.com>
2023-02-20 18:57:40 +0800

    chore: declare strict types

--- src/Story/Result.php
+++ src/Story/Result.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BradieTilley\StoryBoard\Story;
 
 use Throwable;
--- src/Traits/HasPendingContext.php
+++ src/Traits/HasPendingContext.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace BradieTilley\StoryBoard\Traits;
 
 use function BradieTilley\StoryBoard\debug;
