e2f9f985985b7b6a79680bc2beb7696179ca0a5b
Bradie <44430471+bradietilley@users.noreply.github.com>
2023-02-20 21:00:03 +0800

    test: StoryBoardException can statically throw InvalidMagicMethodHandlerException

--- tests/Unit/Exceptions/InvalidMagicMethodHandlerExceptionTest.php
+++ tests/Unit/Exceptions/InvalidMagicMethodHandlerExceptionTest.php
@@ -1,21 +1,28 @@
 <?php
 
 use BradieTilley\StoryBoard\Exceptions\InvalidMagicMethodHandlerException as Error;
+use BradieTilley\StoryBoard\Exceptions\InvalidMagicMethodHandlerException;
+use BradieTilley\StoryBoard\Exceptions\StoryBoardException;
 
-test('can instantiate a InvalidMagicMethodHandlerException for a missing property handler', function () {
+test('can instantiate an InvalidMagicMethodHandlerException for a missing property handler', function () {
     $exception = new Error('example', Error::TYPE_PROPERTY);
 
     expect($exception->getMessage())->toBe('Failed to locate the `$example` magic property shandler');
 });
 
-test('can instantiate a InvalidMagicMethodHandlerException for a missing method handler', function () {
+test('can instantiate an InvalidMagicMethodHandlerException for a missing method handler', function () {
     $exception = new Error('example', Error::TYPE_METHOD);
 
     expect($exception->getMessage())->toBe('Failed to locate the `example()` magic method shandler');
 });
 
-test('can instantiate a InvalidMagicMethodHandlerException for a missing static method handler', function () {
+test('can instantiate an InvalidMagicMethodHandlerException for a missing static method handler', function () {
     $exception = new Error('example', Error::TYPE_STATIC_METHOD);
 
     expect($exception->getMessage())->toBe('Failed to locate the `::example()` magic static method shandler');
 });
+
+
+test('can thrown an InvalidMagicMethodHandlerException via StoryBoardException', function () {
+    throw StoryBoardException::invalidMagicMethodHandlerException('something', 'property');
+})->throws(InvalidMagicMethodHandlerException::class, 'Failed to locate the `$something` magic property shandler');
