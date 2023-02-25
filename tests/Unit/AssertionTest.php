8651419530c0c61a84819c5661274ce3db19f6c0
Bradie <44430471+bradietilley@users.noreply.github.com>
2023-02-25 16:10:47 +0800

    test: assertions can be defined and added in various ways

--- tests/Unit/AssertionTest.php
+++ tests/Unit/AssertionTest.php
@@ -3,8 +3,9 @@
 use BradieTilley\StoryBoard\Exceptions\AssertionNotSpecifiedException;
 use BradieTilley\StoryBoard\Exceptions\ExpectationNotSpecifiedException;
 use BradieTilley\StoryBoard\Story;
+use BradieTilley\StoryBoard\Story\Assertion;
 
-test('a story must have at least one assertion', function () {
+test('a story must have at least one expectation', function () {
     $story = Story::make()
         ->action(fn () => null)
         ->assert(fn () => true)
@@ -17,6 +18,21 @@ test('a story must have at least one assertion', function () {
         $story->run();
     }
 })->throws(ExpectationNotSpecifiedException::class, 'No expectation was found for the story `parent child`');
+
+test('a story works with one expectation', function () {
+    $story = Story::make()
+        ->can()
+        ->action(fn () => null)
+        ->assert(fn () => true)
+        ->name('parent')
+        ->stories([
+            Story::make('child'),
+        ]);
+
+    foreach ($story->allStories() as $story) {
+        $story->run();
+    }
+})->throwsNoExceptions();
 
 test('a story must have at least one assertion checker', function () {
     $story = Story::make()->action(fn () => null)->can()->name('parent')->stories(
@@ -63,4 +79,55 @@ test('you may create a story with an assertion and unset the assertion for a chi
     ];
 
     expect($actual)->toBe($expect);
+});
+
+test('assertions can be defined in various ways', function () {
+    $ran = collect();
+
+    $assertion1 = Assertion::make('assertion_1')->as(fn () => $ran[] = '1');
+
+    Assertion::make('assertion_2')->as(fn () => $ran[] = '2');
+    $assertion2 = 'assertion_2';
+
+    $assertion3 = fn () => $ran[] = '3';
+
+    $story = Story::make()
+        ->can()
+        ->assert($assertion1)
+        ->assert($assertion2)
+        ->assert($assertion3)
+        ->action(fn () => $ran[] = 'action')
+        ->boot()
+        ->perform();
+
+    expect($ran->toArray())->toBe([
+        'action',
+        '1',
+        '2',
+        '3',
+    ]);
+});
+
+test('assertions can be added to a story in various ways', function () {
+    $ran = collect();
+
+    $story = Story::make()
+        ->can()
+        ->action(fn () => null)
+        ->assert(fn () => $ran[] = 'assert')
+        ->setAssertion(fn () => $ran[] = 'setAssertion')
+        ->setAssertions([
+            fn () => $ran[] = 'setAssertions',
+        ])
+        ->whenCan(fn () => $ran[] = 'whenCan')
+        ->whenCannot(fn () => $ran[] = 'whenCannot')
+        ->boot()
+        ->perform();
+
+    expect($ran->toArray())->toBe([
+        'assert',
+        'setAssertion',
+        'setAssertions',
+        'whenCan',
+    ]);
 });
