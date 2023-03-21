f97fd5e8934ebfd1bd9138f1b712da42cb646702
Bradie <44430471+bradietilley@users.noreply.github.com>
2023-03-21 17:40:26 +0800

    test: improve alias tests

--- tests/Unit/AliasesTest.php
+++ tests/Unit/AliasesTest.php
@@ -16,41 +16,56 @@ use Tests\Mocks\MockStory;
 
 test('you can create a story using the story function while utilising a custom alias', function () {
     // Shouldn't be a mock instance
-    expect(story())->not->toBeInstanceOf(MockStory::class);
+    expect(get_class(story('something')))->toBe(Story::class);
+    expect(get_class(Story::make('something')))->toBe(Story::class);
 
     // Set it to be a mock instance
     StoryAliases::setClassAlias(Story::class, MockStory::class);
-    expect(story())->toBeInstanceOf(MockStory::class);
+
+    expect(get_class(story('something')))->toBe(MockStory::class);
+    expect(get_class(Story::make('something')))->toBe(MockStory::class);
 
     // Set it back to the original
     StoryAliases::setClassAlias(Story::class, Story::class);
-    expect(story())->not->toBeInstanceOf(MockStory::class);
+
+    expect(get_class(story('something')))->toBe(Story::class);
+    expect(get_class(Story::make('something')))->toBe(Story::class);
 });
 
 test('you can create an action using the action function while utilising a custom alias', function () {
     // Shouldn't be a mock instance
-    expect(action('do_something'))->not->toBeInstanceOf(MockAction::class);
+    expect(get_class(action('do_something')))->toBe(Action::class);
+    expect(get_class(Action::make('do_something')))->toBe(Action::class);
 
     // Set it to be a mock instance
     StoryAliases::setClassAlias(Action::class, MockAction::class);
-    expect(action('do_something'))->toBeInstanceOf(MockAction::class);
+
+    expect(get_class(action('do_something')))->toBe(MockAction::class);
+    expect(get_class(Action::make('do_something')))->toBe(MockAction::class);
 
     // Set it back to the original
     StoryAliases::setClassAlias(Action::class, Action::class);
-    expect(action('do_something'))->not->toBeInstanceOf(MockAction::class);
+
+    expect(get_class(action('do_something')))->toBe(Action::class);
+    expect(get_class(Action::make('do_something')))->toBe(Action::class);
 });
 
 test('you can create an assertion using the assertion function while utilising a custom alias', function () {
     // Shouldn't be a mock instance
-    expect(assertion('expect_something'))->not->toBeInstanceOf(MockAssertion::class);
+    expect(get_class(assertion('expect_something')))->toBe(Assertion::class);
+    expect(get_class(Assertion::make('expect_something')))->toBe(Assertion::class);
 
     // Set it to be a mock instance
     StoryAliases::setClassAlias(Assertion::class, MockAssertion::class);
-    expect(assertion('expect_something'))->toBeInstanceOf(MockAssertion::class);
+    
+    expect(get_class(assertion('expect_something')))->toBe(MockAssertion::class);
+    expect(get_class(Assertion::make('expect_something')))->toBe(MockAssertion::class);
 
     // Set it back to the original
     StoryAliases::setClassAlias(Assertion::class, Assertion::class);
-    expect(assertion('expect_something'))->not->toBeInstanceOf(MockAssertion::class);
+
+    expect(get_class(assertion('expect_something')))->toBe(Assertion::class);
+    expect(get_class(Assertion::make('expect_something')))->toBe(Assertion::class);
 });
 
 test('you cannot set an alias to a class that does not exist', function () {
