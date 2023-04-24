b1b0a31953cb344060017c86a65cc61c4627ed99
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2023-04-24 15:46:30 +0800

    test: ensure test starts with empty static array

--- tests/Feature/ActionBootTest.php
+++ tests/Feature/ActionBootTest.php
@@ -6,8 +6,9 @@ use Tests\Fixtures\AnExampleActionWithTraits;
 use function BradieTilley\Stories\Helpers\story;
 
 test('traits on an action class are booted', function () {
+    AnExampleActionWithTraits::$ran = [];
+    
     Story::setInstance(story());
-
     story()->action(AnExampleActionWithTraits::class);
 
     expect(AnExampleActionWithTraits::$ran)->toBe([
