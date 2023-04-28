88c8c9512c52b8afb525609b541c34077ce35813
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2023-04-29 07:26:19 +0800

    test: add test that ensures you can expect against a story variable

--- tests/Feature/ActionExpectsTest.php
+++ tests/Feature/ActionExpectsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use BradieTilley\Stories\Concerns\Stories;
+use function BradieTilley\Stories\Helpers\story;
 use BradieTilley\Stories\Story;
 use PHPUnit\Framework\ExpectationFailedException;
 
@@ -21,3 +22,15 @@ test('an expectation chain can fail')
     ->throws(ExpectationFailedException::class, 'Failed asserting that 123 is identical to 456.')
     ->expects('abc')
     ->toBe(456);
+
+test('an expectation chain can be built within an action')
+    ->action(function () {
+        story()->set('abc', 123);
+        story()->set('def', 456);
+        story()->set('ghi', 789);
+    })
+    ->action(function () {
+        story()->expects('abc')->toBe(123);
+        story()->expects('def')->toBe(456);
+        story()->expects('ghi')->toBe(789);
+    });
