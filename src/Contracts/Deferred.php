4f5da27c7641396b43f8c81593ea96f5fcd777e5
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2023-04-29 07:54:58 +0800

    docs: add information about the interface and what it does

--- src/Contracts/Deferred.php
+++ src/Contracts/Deferred.php
@@ -2,6 +2,19 @@
 
 namespace BradieTilley\Stories\Contracts;
 
+/**
+ * Add this interface to an action that you don't want to be computed immediately.
+ *
+ * The callbacks (before/after) and `__invoke` method will always be deferred until
+ * the story boots this action (after test case is set up), but any chained methods
+ * would otherwise be invoked immediately.
+ *
+ * Example:
+ *
+ *  MyStandardAction::make()->withSomething() // 'withSomething' is immediately run
+ *
+ *  MyDeferredAction::make()->withSomething() // 'withSomething' is not immediately run
+ */
 interface Deferred
 {
 }
