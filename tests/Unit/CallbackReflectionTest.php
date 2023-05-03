331672cb091fd110c23ca5b44f48e3fedc8a811e
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2023-05-03 17:10:26 +0800

    refactor: generate a more descriptive random name for inline closure actions by including file path/line

--- tests/Unit/CallbackReflectionTest.php
+++ tests/Unit/CallbackReflectionTest.php
@@ -3,6 +3,7 @@
 use BradieTilley\Stories\Action;
 use BradieTilley\Stories\Exceptions\FailedToIdentifyCallbackArgumentsException;
 use BradieTilley\Stories\Helpers\CallbackReflection;
+use Illuminate\Support\Str;
 
 test('a callback reflection can identify arguments of a closure', function () {
     $callable = function (string $abc, int $def, bool $ghi) {
@@ -68,3 +69,20 @@ test('cannot reflect a class method that is invalid', function () {
     FailedToIdentifyCallbackArgumentsException::class,
     'Failed to identify callback arguments: Callback reflection format not supported',
 );
+test('can generate a unique name for a closure', function () {
+    $closure = function () {
+
+    };
+
+    $line = __LINE__ - 4;
+    $file = __FILE__;
+
+    $expect = "inline@{$file}:{$line}";
+    $callback = CallbackReflection::make($closure);
+
+    $actual = $callback->uniqueName();
+    expect($actual)->toStartWith($expect);
+
+    $actual = Str::replaceFirst($expect, '', $actual);
+    expect($actual)->toMatch('/^\[[a-z0-9]{8}\]$/i');
+});
