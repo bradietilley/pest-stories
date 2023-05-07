7dd6be24678557b45c4f1590c9f5f12f7a1fa683
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2023-05-07 12:20:05 +0800

    test: remove redundant intermittent failing tests

--- tests/Unit/InvokerTest.php
+++ tests/Unit/InvokerTest.php
@@ -8,12 +8,9 @@ use BradieTilley\Stories\Helpers\StoryInvoker;
 use BradieTilley\Stories\Story;
 use Illuminate\Container\Container as ApplicationContainer;
 use Illuminate\Contracts\Container\Container;
-use Illuminate\Support\Benchmark;
 use Illuminate\Support\Collection;
 use Tests\Fixtures\AnExampleAction;
-use Tests\Fixtures\AnExampleActionWithManyArguments;
 use Tests\Fixtures\AnExampleClassWithPrivateMethod;
-use Tests\Fixtures\NonActionExample;
 
 test('invoker can invoke a basic closure', function () {
     $ran = Collection::make();
@@ -188,68 +185,6 @@ test('can invoke methods on objects', function (string $visibility, string $stat
     'protected static method' => ['protected', 'static', 5, 'Cannot call non-callable callback: method: `Tests\Fixtures\AnExampleClassWithPrivateMethod::protectedInvokeMeStatic()`'],
     'public static method' => ['public', 'static', 6, null],
 ]);
-
-$benchmarks = Collection::make([]);
-
-test('a story invoker benchmark test', function () use ($benchmarks) {
-    $callback = [AnExampleActionWithManyArguments::make(), '__invoke'];
-    $arguments = [
-        'abc' => 3534,
-        'def' => 'erg erjng ierng iuerngeiru',
-        'ghi' => new NonActionExample(),
-        'jkl' => AnExampleAction::make(),
-    ];
-
-    $invoker = new StoryInvoker();
-
-    $avg = Benchmark::measure(fn () => $invoker->call($callback, $arguments), iterations: 1000);
-    $benchmarks['invoker'] = $avg;
-
-    expect(true)->toBeTrue();
-});
-
-test('a container invoker benchmark test', function () use ($benchmarks) {
-    $callback = [AnExampleActionWithManyArguments::make(), '__invoke'];
-    $arguments = [
-        'abc' => 3534,
-        'def' => 'erg erjng ierng iuerngeiru',
-        'ghi' => new NonActionExample(),
-        'jkl' => AnExampleAction::make(),
-    ];
-
-    $invoker = ApplicationContainer::getInstance();
-
-    $avg = Benchmark::measure(fn () => $invoker->call($callback, $arguments), iterations: 1000);
-    $benchmarks['container'] = $avg;
-
-    expect(true)->toBeTrue();
-});
-
-test('the invoker runs quicker than the laravel container - invoker benchmark test', function () use ($benchmarks) {
-    /**
-     * Skip this test when xdebug coverage is enabled,
-     */
-    if (env('XDEBUG_MODE', ini_get('xdebug.mode')) === 'coverage') {
-        expect(true)->toBeTrue();
-
-        return;
-    }
-
-    expect($benchmarks)->toHaveCount(2)->toHaveKeys([
-        'invoker',
-        'container',
-    ]);
-
-    expect($benchmarks['invoker'])
-        ->toBeLessThan(
-            $benchmarks['container'],
-            sprintf(
-                'Failed asserting the invoker (ran in %ss) was quicker than the container (ran in %ss)',
-                $benchmarks['invoker'],
-                $benchmarks['container'],
-            )
-        );
-});
 
 test('you can enable laravel container invoker using helper function', function () {
     story()->fresh()->use();
